57c87cd2cb4c35614dc511158c454e7caf1bc0ee
Nariman Palangi <nariman.palangi70@gmail.com>
2020-04-26 09:19:34 +0430

    First commit
    Signed-off-by: Nariman Palangi <nariman.palangi70@gmail.com>

---
+++ vendors/Chart.js/dist/custom.js
@@ -0,0 +1,29 @@
+
+
+    // Line chart
+
+    if ($('#lineChart').length) {
+
+        var ctx = document.getElementById("lineChart");
+        var lineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ["1", "2", "3", "4", "5", "6", "7"],
+                datasets: [{
+                    label: "CHART",
+                    backgroundColor: "rgba(38, 185, 154, 0.31)",
+                    borderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBorderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointBorderWidth: 1,
+                    data: [31, 74, 6, 39, 20, 85, 7]
+                }]
+            },
+        });
+
+    }
+
+
+
